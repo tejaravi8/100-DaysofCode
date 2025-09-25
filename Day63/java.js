dd6aa57fb1e091ba6fa5c2fc1082dacac7a81a34
tejaravi8 <botsaraviteja@gmail.com>
2025-09-25 23:21:47 +0530

    day63

---
+++ Day63/java.js
@@ -0,0 +1,39 @@
+// // // // // // console.log(x); // undefined
+// // // // // // // var x = 10;
+
+// // // // // // console.log(a);
+// // // // // // const a=5;
+
+// // // // // const obj = {name: 'JS'}; 
+// // // // // console.log(obj.name);
+
+// // // // function example() {
+// // // //  var message = "Hello!";
+// // // //  console.log(message); // Works fine
+// // // // }
+// // // // var message = "Hello!";
+// // // // console.log(message); // ReferenceError: message is not defined
+
+// // // // const a=10;
+// // // // a=11;
+// // // // console.log(a);
+
+// // // var a=10;
+// // // let a=4;
+// // // // {  let b = 2; const c = 3; }
+// // // console.log(a);
+// // // //  a) a b) b c) c d) All throw err
+
+// // var x = 5; 
+// // { var x = 10; } 
+// // console.log(x); 
+// // const a;
+// // a = 10;
+// var a = 1; 
+// let a=2;
+// // { let a = 2;console.log(a); }
+// // console.log(a)
+let x = 2;
+var x = 10;
+
+console.log(x);
