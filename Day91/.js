72556d6b87f7c08ab78c74eaa69074d4bfb37a2e
tejaravi8 <botsaraviteja@gmail.com>
2025-10-24 12:47:38 +0530

    code

---
+++ Day91/.js
@@ -0,0 +1,19 @@
+// function one(callback){
+//     console.log(callback())
+// }
+
+// one(function(){
+//     return "hello javascript"
+// })
+
+
+// function first(func){
+//     console.log("hehe")
+//     return func()
+// }
+
+// let fun=function(){
+//     console.log("elleh")
+// }
+
+// first(fun)
