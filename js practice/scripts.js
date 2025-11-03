fbbcad1b81d06a00d3e4366d3ceb3ce4c4aa2600
tejaravi8 <botsaraviteja@gmail.com>
2025-11-03 12:14:19 +0530

    js practice

---
+++ js practice/scripts.js
@@ -0,0 +1,64 @@
+// for(let i=1;i<=2;i++){
+//     for(let j=1 ;j<=4;j++){
+//         console.log(i,j)
+//     }
+// }
+
+// for(let i = 1; i <= 3; i++){
+//   for(let j = 1; j <= i; j++){
+//     console.log(i,j)
+//   }
+// }
+
+// for(let i = 1; i <= 3; i++){
+//   for(let j = 1; j <= 3; j++){
+//     if(j === 2) {continue};
+//     console.log(i, j);
+//   }
+// }
+
+// for(let i = 1; i <= 3; i++){
+//   for(let j = 1; j <= 3; j++){
+//     if(j === 2) break;
+//     console.log(i, j);
+//   }
+// }
+
+// for(let i = 2; i < 3; i++){
+//   for(let j = 0; i < 3; j++){
+//     console.log(i, j);
+//     i++
+//   }
+// }
+
+// for(let i = 3; i > 0; i--){
+//   for(let j = i; j > 0; j--){
+//     console.log(i, j);
+//   }
+// }
+
+// for(let i = 0; i < 3; i++){
+//   for(let j = 0; j < 2; j++){
+//     console.log(i, j);
+//     i++;
+//   }
+// }
+
+// for(let i = 1; i <= 3; i++){
+//   for(let j = 1; j <= 3; j++){
+//     console.log(i * j);
+//   }
+// }
+
+// for(let i = 1; i <= 3; i++){
+//   for(let j = i; j <= 3; j++){
+//     console.log(i, j);
+//   }
+// }
+
+for(let i = 1; i <= 3; i++){
+  for(let j = 1; j <= 3; j++){
+    if(i + j > 3) console.log(i, j);
+  }
+}
+
